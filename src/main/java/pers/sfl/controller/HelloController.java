a37efc5bd8d0202efcdd83abe07efae9da466219
小良 <1546369127@qq.com>
2019-05-24 18:05:27 +0800

    add index and bootstrp source

--- src/main/java/pers/sfl/controller/HelloController.java
+++ src/main/java/pers/sfl/controller/HelloController.java
@@ -1,5 +1,6 @@
 package pers.sfl.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Scott  fl_6145@163.com
  * @create 2019-05-24 15:06
  */
-@RestController
+@Controller
 public class HelloController {
-    @GetMapping(value = "hello")
-    public String hello(@RequestParam("name") String name) {
-        return "欢迎您：" + name;
+    @GetMapping(value = "/")
+    public String hello() {
+        return "index";
     }
 }
